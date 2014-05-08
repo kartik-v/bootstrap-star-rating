3d1901bdcc1d368457c71cc58845d592e65d2d47
Kartik Visweswaran <kartikv2@gmail.com>
2014-05-08 12:32:18 +0530

    Fix #9, #10: Cloning of input corrected

--- js/star-rating.js
+++ js/star-rating.js
@@ -80,9 +80,7 @@
             self.$element.on("change", function (e) {
                 if (!self.inactive) {
                     self.setStars();
-                    self.$elementOrig.val(self.$element.val());
-                    self.$elementOrig.trigger('change');
-                    self.$elementOrig.trigger('rating.change', [self.$element.val(), self.$caption.html()]);
+                    self.$element.trigger('rating.change', [self.$element.val(), self.$caption.html()]);
                 }
             });
             self.$clear.on("click", function (e) {
@@ -90,18 +88,18 @@
                     self.clear();
                 }
             });
-            $(self.$elementOrig[0].form).on("reset", function (e) {
+            $(self.$element[0].form).on("reset", function (e) {
                 if (!self.inactive) {
                     self.reset();
                 }
             });
         },
         /**
-         * Initializes and generates a slider if the input type is not a range
+         * Initializes and converts the input to a range/slider input
          */
         initSlider: function (options) {
             var self = this,
-                id = isEmpty(self.$elementOrig.attr('id')) ? uniqId() : 'kvstar-' + self.$elementOrig.attr('id');
+                id = isEmpty(self.$elementOrig.attr('id')) ? uniqId() : self.$elementOrig.attr('id');
             if (isEmpty(self.$elementOrig.val())) {
                 self.$elementOrig.val(0);
             }
@@ -118,12 +116,12 @@
             if (isNaN(self.step) || isEmpty(self.step) || self.step == 0) {
                 self.step = DEFAULT_STEP;
             }
-            self.$elementOrig.clone(true).attr({'id': id, 'name': id, 'type': 'range'}).insertBefore(self.$elementOrig);
-            self.$elementOrig.removeAttr('class');
+            var val = self.$elementOrig.val();
+            self.$elementOrig.clone(true).attr({'id': id, 'type': 'range'}).insertBefore(self.$elementOrig);
+            self.$elementOrig.remove();
             self.$element = $('#' + id);
             self.$element.attr({min: self.min, max: self.max, step: self.step, disabled: self.disabled, readonly: self.readonly});
-            self.$element.val(self.$elementOrig.val());
-            self.$elementOrig.hide();
+            self.$element.val(val);
         },
         /**
          * Polyfills and generates a simple select input for browsers not
@@ -297,17 +295,14 @@
                 self.$caption.html(title);
             }
             self.$element.val(self.clearValue);
-            self.$elementOrig.val(self.clearValue);
-            self.$elementOrig.trigger('change');
             self.setStars();
-            self.$elementOrig.trigger('rating.clear');
+            self.$element.trigger('rating.clear');
         },
         reset: function () {
             var self = this;
             self.$element.val(self.initialValue);
-            self.$elementOrig.val(self.initialValue);
             self.setStars();
-            self.$elementOrig.trigger('rating.reset');
+            self.$element.trigger('rating.reset');
         },
         update: function (val) {
             if (arguments.length > 0) {

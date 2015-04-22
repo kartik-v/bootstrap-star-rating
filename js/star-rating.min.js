36b83e3d2284a73983e374eb127dd5fe5f61aafb
Kartik Visweswaran <kartikv2@gmail.com>
2015-04-22 23:48:01 +0530

    Update to release v3.5.2

--- js/star-rating.min.js
+++ js/star-rating.min.js
@@ -1,6 +1,6 @@
 /*!
  * @copyright &copy; Kartik Visweswaran, Krajee.com, 2013 - 2015
- * @version 3.5.1
+ * @version 3.5.2
  *
  * A simple yet powerful JQuery star rating plugin that allows rendering
  * fractional star ratings and supports Right to Left (RTL) input.

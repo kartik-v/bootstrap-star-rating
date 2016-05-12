a0396bd356157a657045e7d2ac93965c4541525d
Giulio Ganci <giulioganci@gmail.com>
2016-05-12 10:00:39 +0200

    Create italian translation

---
+++ js/star-rating_locale_it.js
@@ -0,0 +1,31 @@
+/*!
+ * Star Rating italian Translations
+ *
+ * This file must be loaded after 'star-rating.js'. Patterns in braces '{}', or
+ * any HTML markup tags in the messages must not be converted or translated.
+ *
+ * @see http://github.com/kartik-v/bootstrap-star-rating
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+(function ($) {
+    "use strict";
+    $.fn.ratingLocales['it'] = {
+        defaultCaption: '{rating} Stelle',
+        starCaptions: {
+            0.5: 'Mezza Stella',
+            1: 'Una Stella',
+            1.5: 'Una Stella & Mezzo',
+            2: 'Due Stelle',
+            2.5: 'Due Stelle & Mezzo',
+            3: 'Tre Stelle',
+            3.5: 'Tre Stelle & Mezzo',
+            4: 'Quattro Stelle',
+            4.5: 'Quattro Stelle & Mezzo',
+            5: 'Five Stars'
+        },
+        clearButtonTitle: 'Rimuovi',
+        clearCaption: 'Nessuna valutazione'
+    };
+})(window.jQuery);
